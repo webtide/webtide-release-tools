a56089c4e91e5f67e84209a87fc22b900575c6a3
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2021-06-08 11:31:50 -0500

    Fixing testcase

--- src/test/java/net/webtide/tools/release/GitCacheTest.java
+++ src/test/java/net/webtide/tools/release/GitCacheTest.java
@@ -17,7 +17,6 @@ import java.nio.file.Path;
 import java.util.Set;
 
 import org.eclipse.jgit.api.Git;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,16 +25,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class GitCacheTest
 {
     @Test
-    @Disabled("Not functioning yet")
     public void testGitCache() throws IOException
     {
+        String sha = "adee2dd17af1561057e5857637c4f12c87fd3b43";
         Path cloneDir = GitUtil.findGitRoot();
         Git git = Git.open(cloneDir.toFile());
         GitCache cache = new GitCache(git);
-        Set<String> diffPaths = cache.getPaths("101d803ee281bf1de8acb71f76169ba39ca2806b");
-        assertEquals(3, diffPaths.stream().filter((filename) -> filename.startsWith("src/")).count());
-        Set<String> branchesContaining =
-            cache.getBranchesContaining("101d803ee281bf1de8acb71f76169ba39ca2806b");
+        Set<String> diffPaths = cache.getPaths(sha);
+        assertEquals(27, diffPaths.stream().filter((filename) -> filename.startsWith("src/")).count());
+        Set<String> branchesContaining = cache.getBranchesContaining(sha);
         assertNotNull(branchesContaining);
     }
 }
